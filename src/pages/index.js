44893cd0441652c348a5492034f3a89e16b7bf5b
nikhil <ngupta.esaral@gmail.com>
2024-06-29 18:51:33 +0530

    ..

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react"
 const Page = () => {
     const [data, setData] = useState([])
     useEffect(() => {
-        fetch("https:development.esaral.com/v1/mentorship/get-spayee-user-courses?spayee_id=66641f9b328c7c6a58e174a4")
+        fetch("https://development.esaral.com/v1/mentorship/get-spayee-user-courses?spayee_id=66641f9b328c7c6a58e174a4")
             .then(res => res.json())
             .then(data => setData(data))
             .catch(alert)
